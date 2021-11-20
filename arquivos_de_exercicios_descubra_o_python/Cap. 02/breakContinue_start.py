985502c15acc6a0465d4b1342c0208fc085bdc54
Jota Gouveia <jotagouveia@gmail.com>
2021-11-20 13:50:23 -0300

    Comitar

--- arquivos_de_exercicios_descubra_o_python/Cap. 02/breakContinue_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 02/breakContinue_start.py
@@ -1,3 +1,20 @@
 #
 # Exemplo de como usar os comando Break e Continue
 #
+def LoopBreak():
+    for x in range(5, 10):
+        if x == 7:
+            break
+        print ("o valor de x é : ", x)
+
+LoopBreak()
+
+#LoopBreak()
+
+def LoopContinue():
+    for x in range(5,10):
+        if x==7:
+            continue
+        print ("o valor de x é ", x)
+
+LoopContinue()
--- arquivos_de_exercicios_descubra_o_python/Cap. 02/classes_start.py
+++ arquivos_de_exercicios_descubra_o_python/Cap. 02/classes_start.py
@@ -1,3 +1,24 @@
 #
 # Exemplo de como criar classes
 #
+class minhaClasse():
+    def __init__(self):
+        self.meuAtributo = "Passou pelo construtor!"
+
+    def meuMetodo (self):
+        print ("Passou pelo meu método!")
+
+    def meuMetodo2 (self, valor):
+        self.outroAtributo = valor
+        print (self.outroAtributo)
+
+def criaObjetos():
+    meuObj = minhaClasse()
+    var1 = meuObj.meuAtributo
+    print(var1)
+
+    meuObj.meuMetodo()
+
+    meuObj.meuMetodo2 ("Executando um método")
+
+criaObjetos()
